44f050b249c741bca1057ec332dba281c628b4a3
João Pedro Lança <joaolanca53@gmail.com>
2025-08-06 14:06:28 -0300

    feat: add products page

---
+++ src/app/products-variant/[slug]/components/variants-selector.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { productVariantTable } from "@/db/schema";
+
+interface VariantSelectorProps {
+  selectedVariantSlug: string;
+  variants: (typeof productVariantTable.$inferInsert)[];
+}
+
+const VariantSelector = ({
+  selectedVariantSlug,
+  variants,
+}: VariantSelectorProps) => {
+  return (
+    <div className="flex items-center gap-4">
+      {variants.map((variant) => (
+        <Link href={`/products-variant/${variant.slug}`} key={variant.id}>
+          className=
+          {selectedVariantSlug === variant.slug
+            ? "border-primary border-2 roounded-xl"
+            : ""}
+          <Image
+            src={variant.imageUrl}
+            alt={variant.name}
+            width={68}
+            height={68}
+            className="roounded-xl"
+          />
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default VariantSelector;
