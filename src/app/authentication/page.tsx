cd6df371f1394c1935e040bedbcbdae39264fd9f
João Pedro Lança <joaolanca53@gmail.com>
2025-08-15 15:53:28 -0300

    feita a responsividade da página de login para desktops

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -9,29 +9,31 @@ const Authentication = async () => {
     <>
       <Header />
 
-      <div className="flex w-full flex-col gap-4 p-4 sm:gap-6 sm:p-5 lg:gap-8 lg:p-6">
-        <Tabs defaultValue="sign-in" className="w-full">
-          <TabsList className="grid w-full grid-cols-2">
-            <TabsTrigger
-              value="sign-in"
-              className="text-xs sm:text-sm lg:text-base"
-            >
-              Entrar
-            </TabsTrigger>
-            <TabsTrigger
-              value="sign-up"
-              className="text-xs sm:text-sm lg:text-base"
-            >
-              Criar conta
-            </TabsTrigger>
-          </TabsList>
-          <TabsContent value="sign-in" className="w-full">
-            <SignInForm />
-          </TabsContent>
-          <TabsContent value="sign-up" className="w-full">
-            <SignUpForm />
-          </TabsContent>
-        </Tabs>
+      <div className="flex w-full flex-col gap-4 p-4 sm:gap-6 sm:p-5 lg:h-[calc(100vh-80px)] lg:items-center lg:justify-center lg:gap-8 lg:overflow-hidden lg:p-6">
+        <div className="w-full lg:max-w-md">
+          <Tabs defaultValue="sign-in" className="w-full">
+            <TabsList className="grid w-full grid-cols-2">
+              <TabsTrigger
+                value="sign-in"
+                className="text-xs sm:text-sm lg:text-base"
+              >
+                Entrar
+              </TabsTrigger>
+              <TabsTrigger
+                value="sign-up"
+                className="text-xs sm:text-sm lg:text-base"
+              >
+                Criar conta
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="sign-in" className="w-full">
+              <SignInForm />
+            </TabsContent>
+            <TabsContent value="sign-up" className="w-full">
+              <SignUpForm />
+            </TabsContent>
+          </Tabs>
+        </div>
       </div>
     </>
   );
